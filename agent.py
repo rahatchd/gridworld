08e875ac32ad170ff248e5097a37324a84b2ed0b
Rahat Dhande <rahatchd@gmail.com>
2019-04-25 01:11:35 -0700

    quick fix

--- agent.py
+++ agent.py
@@ -92,12 +92,7 @@ class AsyncAgent(Agent):
         self.policy[self.state[0], self.state[1], choice] += reward + self.gamma * next_expected_reward - current_policy
 
     def step(self):
-        choice = self._choose_action()
-        action = self.action_labels[choice]
-        updated_state, reward = self.environment.step(action)
-        self._update_policy(choice, updated_state, reward)
-        self.state = updated_state
-
+        reward = Agent.step(self)
         self.step_num += 1
         if (self.step_num % self.async_update) == 0:
             with self.lock:
